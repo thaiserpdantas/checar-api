6cbaac425d699cf2c8c373e790f9ecbc260e4084
Thaise Dantas <thaiserpdantas@gmail.com>
2020-08-14 18:59:13 -0300

    Resolve conflicts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (request, response) => response.json({ message: 'Hello Node' }));
+
+const port = 3333;
+app.listen(3333, () => {
+    console.log('✨ Server started on port: ', port);
+});
